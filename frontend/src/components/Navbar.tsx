b5e0aa6fbacbc31209fe15507f046a105d7037a4
Harsh <harsh10mahobia@gmail.com>
2025-08-18 23:02:13 +0530

    added header icon, NavBar changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="bg-black p-4 text-white text-xl font-semibold flex justify-between items-center shadow-md">
-      <h2 className="text-2xl tracking-wide">🚌 Bus Ticketing</h2>
+      <h2 className="text-2xl tracking-wide flex justify-between items-baseline gap-x-4">
+        🚌 Swift Seat 
+        <p className="hidden md:block text-lg">- A Bus Ticketing System</p>
+      </h2>
 
       <div className="text-lg flex gap-6">
         <Link
